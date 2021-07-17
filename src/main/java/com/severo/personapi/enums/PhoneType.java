3ec97ec4ec74dc7818969d902a29eea8ad1e4471
robinsonvs <robinsonvs@gmail.com>
2021-07-17 14:57:14 -0300

    add person and phone maps

---
+++ src/main/java/com/severo/personapi/enums/PhoneType.java
@@ -0,0 +1,15 @@
+package com.severo.personapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    NOME("Home"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
+}
