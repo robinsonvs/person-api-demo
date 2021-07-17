106b3e9a60e811f7703e3f37efa13e85381e3139
robinsonvs <robinsonvs@gmail.com>
2021-07-17 16:12:01 -0300

    add refactory controller and created service

--- src/main/java/com/severo/personapi/controller/PersonController.java
+++ src/main/java/com/severo/personapi/controller/PersonController.java
@@ -2,31 +2,26 @@ package com.severo.personapi.controller;
 
 import com.severo.personapi.dto.MessageResponseDTO;
 import com.severo.personapi.entity.Person;
-import com.severo.personapi.repository.PersonRepository;
+import com.severo.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/person")
 public class PersonController {
 
-    private final PersonRepository personRepository;
+    private final PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person) {
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId())
-                .build();
+        return personService.createPerson(person);
     }
 
 }
---
+++ src/main/java/com/severo/personapi/service/PersonService.java
@@ -0,0 +1,26 @@
+package com.severo.personapi.service;
+
+import com.severo.personapi.dto.MessageResponseDTO;
+import com.severo.personapi.entity.Person;
+import com.severo.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
