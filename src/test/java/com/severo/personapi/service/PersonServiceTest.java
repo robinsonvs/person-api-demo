9b5c017831ee6702613f69dbed5e01500fcb4e23
robinsonvs <robinsonvs@gmail.com>
2021-07-19 01:12:57 -0300

    developed tests in PersonServiceTest 01

--- src/test/java/com/severo/personapi/service/PersonServiceTest.java
+++ src/test/java/com/severo/personapi/service/PersonServiceTest.java
@@ -12,11 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static com.severo.personapi.utils.PersonUtils.createFakeDTO;
 import static com.severo.personapi.utils.PersonUtils.createFakeEntity;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
@@ -59,6 +61,28 @@ public class PersonServiceTest {
         assertEquals(expectedPersonDTO, personDTO);
         assertEquals(expectedSavedPerson.getId(), personDTO.getId());
         assertEquals(expectedSavedPerson.getFirstName(), personDTO.getFirstName());
+    }
+
+    @Test
+    void testGivenInvalidPersonIdThenReturnThrowException() {
+        var invalidId = 1L;
+        when(personRepository.findById(invalidId)).thenReturn(Optional.ofNullable(any(Person.class)));
+
+        assertThrows(PersonNotFoundException.class, () -> personService.findById(invalidId));
+    }
+
+    @Test
+    void testGivenNoDataThenReturnAllPersonSaved() {
+        List<Person> expectedSavedPersons = Collections.singletonList(createFakeEntity());
+        PersonDTO personDTO = createFakeDTO();
+
+        when(personRepository.findAll()).thenReturn(expectedSavedPersons);
+        when(personMapper.toDTO(any(Person.class))).thenReturn(personDTO);
+
+        List<PersonDTO> expectedPersonsDTOList = personService.listAll();
+
+        assertFalse(expectedPersonsDTOList.isEmpty());
+        assertEquals(expectedPersonsDTOList.get(0).getId(), personDTO.getId());
     }
 
     private MessageResponseDTO createExpectedMessageResponse(Long id) {
